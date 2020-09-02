55c9e3f5bfe936d55cd42d7f3a818821353484ce
Totale Reelnamme <47120653+mentalburden@users.noreply.github.com>
2020-09-02 08:41:34 -0700

    Update telephone.py

--- telephone.py
+++ telephone.py
@@ -3,23 +3,30 @@
 import sys
 import paramiko
 
-usernm = 'c00luser'
-passwd = 'KewlPassWordThatISNTREAL'
-hostslist = ['10.13.13.10','10.13.13.20','10.13.13.30','10.13.13.40','10.13.13.50']
+usernm = 'root'
+iplist = 'workerlist'
+privatekey = '/root/paramiko/testkey2'
+hostslist = []
+handleprikey = paramiko.ecdsakey.ECDSAKey(filename=privatekey)
 
 def connect(host,cmd):
         cleancmd = "hostname && echo \"---\" && " + cmd + "\n"
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(host, port=420, username=usernm, password=passwd)
+        client.connect(host, username=usernm, pkey=handleprikey)
         stdin, stdout, stderr = client.exec_command(cleancmd)
         for line in stdout.readlines():
                 print(line.replace("\n",""))
-        print('\n')
+                print('\n')
         client.close()
 
 
 #main starts here
 thiscmd = sys.argv[1]
+with open(iplist, 'r+') as hostsfile:
+        for row in hostsfile:
+                hostslist.append(row.rstrip('\n'))
+print(hostslist)
+
 for x in hostslist:
         connect(x,thiscmd)
